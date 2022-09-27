122a06d0b42d5f34331a143837b55f32bab62ecc
Monochrome <zhaoyangmushiyi@gmail.com>
2022-09-27 22:30:14 +0800

    shiro-demo initial.

---
+++ src/main/java/com/monochrome/ShiroRun.java
@@ -0,0 +1,33 @@
+package com.monochrome;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * @author monochrome
+ * @date 2022/9/27
+ */
+public class ShiroRun {
+    public static void main(String[] args) {
+        // 1.初始化获取SecurityManager
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 2.获取subject对象
+        Subject subject = SecurityUtils.getSubject();
+        // 3.创建token对象，web应用用户名密码从页面传递
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "z3");
+        // 4.完成登录
+        try {
+            subject.login(token);
+            System.out.println("login successful");
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            System.out.println("login failed");
+        }
+    }
+}
