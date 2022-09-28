a31acd87f2e961f98dd5f67b08303728b370b3c6
Monochrome <zhaoyangmushiyi@gmail.com>
2022-09-28 21:24:42 +0800

    新增登录时错误信息的异常捕获.

--- src/main/java/com/monochrome/ShiroRun.java
+++ src/main/java/com/monochrome/ShiroRun.java
@@ -2,6 +2,8 @@ package com.monochrome;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
@@ -25,6 +27,12 @@ public class ShiroRun {
         try {
             subject.login(token);
             System.out.println("login successful");
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            System.out.println("user dose not exist");
+        } catch (IncorrectCredentialsException e) {
+            System.out.println("password is wrong");
+            e.printStackTrace();
         } catch (AuthenticationException e) {
             e.printStackTrace();
             System.out.println("login failed");
